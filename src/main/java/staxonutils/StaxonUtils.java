d603fcee4fd02696d6c06da00f6a1941b72a26e6
kyc <itsogkyc@gmail.com>
2018-03-23 20:31:03 +0900

    xml validator 추가중

--- src/main/java/staxonutils/StaxonUtils.java
+++ src/main/java/staxonutils/StaxonUtils.java
@@ -15,64 +15,66 @@ import de.odysseus.staxon.json.JsonXMLInputFactory;
 import de.odysseus.staxon.json.JsonXMLOutputFactory;
 import de.odysseus.staxon.xml.util.PrettyXMLEventWriter;
 
-public class StaxonUtils {  
-    
-    /**  
-     * json string convert to xml string  
-     */  
-    public String json2xml(String json){  
-        StringReader input = new StringReader(json);  
-        StringWriter output = new StringWriter();  
-        JsonXMLConfig config = new JsonXMLConfigBuilder().multiplePI(false).repairingNamespaces(false).build();  
-        try {  
-            XMLEventReader reader = new JsonXMLInputFactory(config).createXMLEventReader(input);  
-            XMLEventWriter writer = XMLOutputFactory.newInstance().createXMLEventWriter(output);  
-            writer = new PrettyXMLEventWriter(writer);  
-            writer.add(reader);  
-            reader.close();  
-            writer.close();  
-        } catch( Exception e){  
-            e.printStackTrace();  
-        } finally {  
-            try {  
-                output.close();  
-                input.close();  
-            } catch (IOException e) {  
-                e.printStackTrace();  
-            }  
-        }  
-        
-        
-        if(output.toString().length()>=38){//remove <?xml version="1.0" encoding="UTF-8"?>  
-        	return output.toString().substring(39);
-        }
-        
-        return output.toString();  
-    }  
-      
-    /**  
-     * xml string convert to json string  
-     */  
-    public String xml2json(String xml){  
-        StringReader input = new StringReader(xml);  
-        StringWriter output = new StringWriter();  
-        JsonXMLConfig config = new JsonXMLConfigBuilder().autoArray(true).autoPrimitive(true).prettyPrint(true).build();  
-        try {  
-            XMLEventReader reader = XMLInputFactory.newInstance().createXMLEventReader(input);  
-            XMLEventWriter writer = new JsonXMLOutputFactory(config).createXMLEventWriter(output);  
-            writer.add(reader);  
-            reader.close();  
-            writer.close();  
-        } catch( Exception e){  
-            e.printStackTrace();  
-        } finally {  
-            try {  
-                output.close();  
-                input.close();  
-            } catch (IOException e) {  
-                e.printStackTrace();  
-            }  
-        }  
-        return output.toString();  
-    }  
-}  
+public class StaxonUtils {
+
+	/**
+	 * json string convert to xml string
+	 */
+	public String json2xml(String json) {
+		StringReader input = new StringReader(json);
+		StringWriter output = new StringWriter();
+		JsonXMLConfig config = new JsonXMLConfigBuilder().multiplePI(false).repairingNamespaces(false).build();
+		try {
+			XMLEventReader reader = new JsonXMLInputFactory(config).createXMLEventReader(input);
+			XMLEventWriter writer = XMLOutputFactory.newInstance().createXMLEventWriter(output);
+			writer = new PrettyXMLEventWriter(writer);
+			writer.add(reader);
+			reader.close();
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "Error";	  //에러 발생시 Error 리턴
+		} finally {
+			try {
+				output.close();
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+		}
+
+		if (output.toString().length() >= 38) {// remove <?xml version="1.0" encoding="UTF-8"?>
+			return output.toString().substring(39);
+		}
+
+		return output.toString();
+	}
+
+	/**
+	 * xml string convert to json string
+	 */
+	public String xml2json(String xml) {
+		StringReader input = new StringReader(xml);
+		StringWriter output = new StringWriter();
+		JsonXMLConfig config = new JsonXMLConfigBuilder().autoArray(true).autoPrimitive(true).prettyPrint(true).build();
+		try {
+			XMLEventReader reader = XMLInputFactory.newInstance().createXMLEventReader(input);
+			XMLEventWriter writer = new JsonXMLOutputFactory(config).createXMLEventWriter(output);
+			writer.add(reader);
+			reader.close();
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				output.close();
+				input.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return output.toString();
+	}
+}
